0a92c969ddd6fe2d4b74d6aa9bdccded72a40c86
Samyuktha Venkatesan <samyu.vj97@gmail.com>
2024-10-25 18:40:32 -0700

    All replacement algorithms implementations

---
+++ replacement.c
@@ -0,0 +1,49 @@
+#include "replacement.h"
+#include "globals.h"
+
+int select_victim_page_fifo() {
+    int fifo_page = 0, least_fifo_counter = replacement_info.fifo_order[0];
+    for(int i = 1; i < MAIN_MEMORY_PAGES; i++) {
+        if(replacement_info.fifo_order[i] < least_fifo_counter) {
+            least_fifo_counter = replacement_info.fifo_order[i];
+            fifo_page = i;
+        }
+    }
+    return fifo_page;
+}
+int select_victim_page_lru() {
+    int last_recenty_used = 0, lru_access_time = memory_map.pages[0]->last_access_time;
+    for(int i = 1; i < MAIN_MEMORY_PAGES; i++) {
+        if(memory_map.pages[i]->last_access_time < lru_access_time) {
+            lru_access_time = memory_map.pages[i]->last_access_time;
+            last_recenty_used = i;
+        }
+    }
+    return last_recenty_used;
+}
+int select_victim_page_lfu() {
+    int last_least_used = 0, lfu_access_count = memory_map.pages[0]->access_count;
+    for(int i = 1; i < MAIN_MEMORY_PAGES; i++) {
+        if(memory_map.pages[i]->access_count < lfu_access_count) {
+            lfu_access_count = memory_map.pages[i]->access_count;
+            last_least_used = i;
+        }
+    }
+    return last_least_used;
+}
+int select_victim_page_mfu() {
+    int last_most_used = 0, mfu_access_count = memory_map.pages[0]->access_count;
+    for(int i = 1; i < MAIN_MEMORY_PAGES; i++) {
+        if(memory_map.pages[i]->access_count > mfu_access_count) {
+            mfu_access_count = memory_map.pages[i]->access_count;
+            last_most_used = i;
+        }
+    }
+    return last_most_used;
+}
+int select_victim_page_random() {
+    return rand() % MAIN_MEMORY_PAGES;
+}
+int select_victim_page() {
+
+}
